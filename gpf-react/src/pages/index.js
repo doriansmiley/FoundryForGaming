78bfac42675aea72adf6f5ebf3fa820feacd195f
dparker <danielstuartparker@gmail.com>
2022-05-03 16:33:54 -0600

    running unity in the react project

--- gpf-react/src/pages/index.js
+++ gpf-react/src/pages/index.js
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 import { Provider } from "react-redux"
 import store from "./store"
+import unity from "./unity"
 
 import Bio from "../components/bio"
 import Layout from "../components/layout"
