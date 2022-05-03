f1b2b3e3c77ca63c765acaf9fbdf5cfa9ff5e23a
dparker <danielstuartparker@gmail.com>
2022-05-03 16:32:50 -0600

    starting to add unity

--- gpf-react/src/pages/index.js
+++ gpf-react/src/pages/index.js
@@ -40,6 +40,7 @@ const BlogIndex = ({ data, location }) => {
   return (
     <Provider store={store}>
       <Layout location={location} title={siteTitle}>
+        <canvas id="unity-canvas" width="100%" height="100%"></canvas>
         <Seo title="All posts" />
         <Bio />
         <ol style={{ listStyle: `none` }}>
---
+++ gpf-react/src/pages/unity.js
@@ -0,0 +1,42 @@
+var buildUrl = "unity"
+var loaderUrl = buildUrl + "/web.loader.js"
+var config = {
+  dataUrl: buildUrl + "/web.data",
+  frameworkUrl: buildUrl + "/web.framework.js",
+  codeUrl: buildUrl + "/web.wasm",
+  streamingAssetsUrl: "StreamingAssets",
+  companyName: "GPF Demos",
+  productName: "Matchmaker",
+  productVersion: "0.1",
+  showBanner: false,
+}
+
+// By default Unity keeps WebGL canvas render target size matched with
+// the DOM size of the canvas element (scaled by window.devicePixelRatio)
+// Set this to false if you want to decouple this synchronization from
+// happening inside the engine, and you would instead like to size up
+// the canvas DOM size and WebGL render target sizes yourself.
+// config.matchWebGLToCanvasSize = false;
+
+var canvas = document.querySelector("#unity-canvas")
+canvas.style.width = "100%"
+canvas.style.height = "100%"
+
+var script = document.createElement("script")
+script.src = loaderUrl
+var unity = null
+script.onload = () => {
+  console.log("lodaed")
+  window["createUnityInstance"](canvas, config, progress => {})
+    .then(unityInstance => {
+      unity = unityInstance
+    })
+    .catch(message => {
+      alert(message)
+    })
+}
+document.body.appendChild(script)
+
+const result = "test"
+
+export default result
