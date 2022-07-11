cb40017a46e53ecd190efcec9e61dcc18ff8fa12
iMAN <imankh@gmail.com>
2022-07-10 19:10:10 -0700

    better handling of bad data in.  improved interface (now using paths).  fixed bad namespace

--- matchmaker/Assets/Scripts/Editor/ExperimentTest.cs
+++ matchmaker/Assets/Scripts/Editor/ExperimentTest.cs
@@ -4,45 +4,74 @@ using GPF;
 using System.Threading.Tasks;
 using GPF.ServerObjects;
 using GPFEditor;
-using ServerObjects.Test;
+using ServerObjects;
 using System.Collections.Generic;
 
 namespace REST
 {
-    [TestFixture]
-    sealed class ExperimentTest: ServerObjectTest
+  [TestFixture]
+  sealed class ExperimentTest : ServerObjectTest
+  {
+    public SOID<ExperimentSO> RESTSOID = Registry.GetId<ExperimentSO>();
+
+    public override async Task Run()
     {
-        public SOID<ExperimentSO> RESTSOID = Registry.GetId<ExperimentSO>();
+      if (RestDisabled)
+        Assert.Inconclusive("This test is only intended to run when REST is enabled");
 
-        public override async Task Run()
-        {
-            if (RestDisabled)
-                Assert.Inconclusive("This test is only intended to run when REST is enabled");
+      var syncer = CreateSyncer("default");
+      ExperimentSO restSO = await syncer.Sync(RESTSOID);
 
-            var syncer = CreateSyncer("default");
-            ExperimentSO restSO = await syncer.Sync(RESTSOID);
+      Debug.Log("Binded to: " + RESTSOID);
 
-            Debug.Log("Binded to: " + RESTSOID);
+      var headers = new Dictionary<string, List<string>>
+          {
+              {"SOID", new List<string> { RESTSOID } }
+          };
 
-            var headers = new Dictionary<string, List<string>>
-            {
-                {"SOID", new List<string> { RESTSOID } }
-            };
+      var badRequest1 = new RESTRequest
+      {
+        Headers = headers,
+        Method = "GET"
+      };
 
-            var req = new RESTRequest
-            {
-                Headers = headers,
-                Method = "GET"
-            };
+      // BAD Path-------------------------------------------------
+      var badResponse1 = await SendREST(badRequest1);
 
-            var response = await SendREST(req, "?foo=bar&bar=foo");
+      Debug.Log("response: " + badResponse1.Body + " Status: " + badResponse1.httpStatusCode);
 
-            Debug.Log("response: " + response.Body);
+      Assert.AreEqual(400, (int)badResponse1.httpStatusCode);
+      Assert.AreEqual("bad path", badResponse1.Body);
 
-            Assert.AreEqual(200, (int)response.httpStatusCode);
-            Assert.AreEqual("2 experiments set", response.Body);
+      // BAD Body-------------------------------------------------
+      var badResponse2 = await SendREST(badRequest1, "set");
 
-            await syncer.WaitFor(restSO, "experiments", FieldIs.WithCount(2) );
-        }
+      Debug.Log("response: " + badResponse2.Body + " Status: " + badResponse2.httpStatusCode);
+
+      Assert.AreEqual(400, (int)badResponse2.httpStatusCode);
+      Assert.AreEqual("bad body", badResponse2.Body);
+
+      var goodRequest = new RESTRequest
+      {
+        Headers = headers,
+        Method = "POST",
+      };
+      var goodResponse = await SendREST(goodRequest, "/reset");
+
+      Assert.AreEqual(200, (int)goodResponse.httpStatusCode);
+
+      Assert.AreEqual("experiments reset", goodResponse.Body);
+
+      Debug.Log("response: " + goodResponse.Body + " Status: " + goodResponse.httpStatusCode);
+
+      // Good Payload should set-------------------------------------------------
+      goodRequest.Body = "foo=bar&bar=foo";
+      var goodResponse2 = await SendREST(goodRequest, "/set");
+
+      Assert.AreEqual(200, (int)goodResponse2.httpStatusCode);
+      Assert.AreEqual("2 experiments set", goodResponse2.Body);
+
+      await syncer.WaitFor(restSO, "experiments", FieldIs.WithCount(2));
     }
+  }
 }
--- matchmaker/Assets/ServerObjects/ExperimentSO.cs
+++ matchmaker/Assets/ServerObjects/ExperimentSO.cs
@@ -1,24 +1,70 @@
 using GPF.ServerObjects;
+using System;
 using System.Collections.Generic;
 
-namespace ServerObjects.Test
+namespace ServerObjects
 {
-    [DataStorePath("sync.experiment")]
-    [Syncable]
-    [Register("experiment")]
-    public class ExperimentSO : RESTBaseSO
-    {
-        public Dictionary<string, string> experiments = new Dictionary<string, string>();
+  [DataStorePath("sync.experiment")]
+  [Syncable]
+  [Register("experiment")]
+  public class ExperimentSO : ServerObject
+  {
+    public Dictionary<string, string> experiments = new Dictionary<string, string>();
 
-        public void Handler(RESTRequestMessage msg)
+    private static Dictionary<string, string> ParseNameVals(string queryString)
+    {
+      var myParams = new Dictionary<string, string>();
+      var pairParts = queryString.Split('&');
+      foreach (var pair in pairParts)
+      {
+        try
         {
-            experiments = GetParamsFromQueryString(msg.Uri.Query);
-      
+          var parts = pair.Split('=');
+          myParams[parts[0]] = parts[1];
+        }
+        catch (Exception)
+        {
+
+        }
+      }
+      return myParams;
+    }
+
+    public void Handler(RESTRequestMessage msg)
+    {
+      string reason = "bad path";
+
+      switch (msg.Uri.AbsolutePath)
+      {
+        case "/set":
+          if (msg.Body.Contains("=") && !msg.Body.Contains("=="))
+          {
+            experiments = ParseNameVals(msg.Body);
             Send(msg.evt.source, new RESTResponseMessage
             {
               Body = $"{experiments.Count} experiments set",
             });
+            return;
+          }
+          reason = "bad body";
+          break;
+        case "/reset":
+          experiments = new Dictionary<string, string>();
 
-        }
+          Send(msg.evt.source, new RESTResponseMessage
+          {
+            Body = "experiments reset",
+          });
+          return;
+      }
+
+      // Not handled
+      Send(msg.evt.source, new RESTResponseMessage
+      {
+        httpStatusCode = System.Net.HttpStatusCode.BadRequest,
+        Body = reason
+      }); 
+
     }
+  }
 }
