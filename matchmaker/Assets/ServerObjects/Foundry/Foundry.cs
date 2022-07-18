5cd77fe327720c927c0011c79211c4daeca07249
dparker <danielstuartparker@gmail.com>
2022-07-18 13:21:10 -0600

    Adding ENV var for foundry specifically

--- matchmaker/Assets/ServerObjects/Foundry/Foundry.cs
+++ matchmaker/Assets/ServerObjects/Foundry/Foundry.cs
@@ -7,85 +7,85 @@ using System.Threading.Tasks;
 
 namespace ServerObjects
 {
-    public static class Foundry
+  public static class Foundry
+  {
+    public const string URL = "https://bright-app-v2ivb.cloud.serverless.com/events";
+
+    public class Event
     {
-        public const string URL = "https://bright-app-v2ivb.cloud.serverless.com/events";
+      public const string SchemaVersion = "0.0.01";
 
-        public class Event
-        {
-            public const string SchemaVersion = "0.0.01";
+      public string uid;
+      public string ts;
+      public string sessionId;
+      public string deviceID;
+      public string platform;
+      public string version = SchemaVersion;
+      public string language;
+      public string device;
+      public string resolution;
+      public Dictionary<string, object> evtAttributes = new Dictionary<string, object>();
+    }
 
-            public string uid;
-            public string ts;
-            public string sessionId;
-            public string deviceID;
-            public string platform;
-            public string version = SchemaVersion;
-            public string language;
-            public string device;
-            public string resolution;
-            public Dictionary<string, object> evtAttributes = new Dictionary<string, object>();
-        }
+    class ServerMessage
+    {
+      public List<Event> events = new List<Event>();
+    }
 
-        class ServerMessage
-        {
-            public List<Event> events = new List<Event>();
-        }
+    public static async Task SendAnalytics(AnalyticsMessage message, Dictionary<string, object> evtAttributes, string sessionId, string PublicID, ulong UpdateOrder, ServerObjectEnv Env, IGPFLogger Logger)
+    {
+      var uid = $"{PublicID}-{UpdateOrder}";
+      var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      var secondsSinceEpoch = Convert.ToInt64((message.timestamp - epoch).TotalMilliseconds);
+      var e = new Event
+      {
+        uid = uid,
+        ts = secondsSinceEpoch.ToString(),
+        sessionId = sessionId,
+        deviceID = message.DeviceId,
+        platform = message.Platform,
+        language = message.Language,
+        device = message.DeviceName,
+        resolution = message.Resolution,
+      };
+      e.evtAttributes = evtAttributes;
 
-        public static async Task SendAnalytics(AnalyticsMessage message, Dictionary<string, object> evtAttributes, string sessionId, string PublicID, ulong UpdateOrder, ServerObjectEnv Env, IGPFLogger Logger)
-        {
-            var uid = $"{PublicID}-{UpdateOrder}";
-            var epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-            var secondsSinceEpoch = Convert.ToInt64((message.timestamp - epoch).TotalMilliseconds);
-            var e = new Event
-            {
-                uid = uid,
-                ts = secondsSinceEpoch.ToString(),
-                sessionId = sessionId,
-                deviceID = message.DeviceId,
-                platform = message.Platform,
-                language = message.Language,
-                device = message.DeviceName,
-                resolution = message.Resolution,
-            };
-            e.evtAttributes = evtAttributes;
+      await SendAnalytics(e, Env, Logger);
+    }
 
-            await SendAnalytics(e, Env, Logger);
-        }
-
-        static async Task SendAnalytics(Event e, ServerObjectEnv Env, IGPFLogger Logger)
-        {
-            var serverMessage = new ServerMessage();
-            serverMessage.events.Add(e);
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(serverMessage);
-      /*
-            if (Env[ServerObjectEnv.Included.BACKEND] == "Simulation")
-            {
-                Logger.Log("Event: " + json);
-            }
-            else
-                await SendRequest(json);
-      */
+    static async Task SendAnalytics(Event e, ServerObjectEnv Env, IGPFLogger Logger)
+    {
+      var serverMessage = new ServerMessage();
+      serverMessage.events.Add(e);
+      var json = Newtonsoft.Json.JsonConvert.SerializeObject(serverMessage);
+      if (Env["FOUNDRY_ENABLED"].ToLower() == "true")
+      {
+        await SendRequest(json);
+      }
+      else
+      {
+        Logger.Log("Event: " + json);
+      }
       Logger.Log("Event: " + json);
     }
 
-        static async Task SendRequest(string json)
-        {
-            var uri = new Uri(URL);
+    static async Task SendRequest(string json)
+    {
+      var uri = new Uri(URL);
 
-            HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, uri);
-            message.Headers.Add("Accept", "application/json");
+      HttpRequestMessage message = new HttpRequestMessage(HttpMethod.Post, uri);
+      message.Headers.Add("Accept", "application/json");
 
-            var data = new StringContent(json, Encoding.UTF8, "application/json");
-            message.Content = data;
+      var data = new StringContent(json, Encoding.UTF8, "application/json");
+      message.Content = data;
 
-            HttpResponseMessage response;
-            using (var client = new HttpClient())
-            {
-                response = await client.SendAsync(message);
-            }
+      HttpResponseMessage response;
+      using (var client = new HttpClient())
+      {
+        response = await client.SendAsync(message);
+      }
 
-            response.EnsureSuccessStatusCode();
-        }
+      response.EnsureSuccessStatusCode();
     }
+  }
 }
