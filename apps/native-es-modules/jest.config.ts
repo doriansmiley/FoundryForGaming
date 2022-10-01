dd27f2aac44c59c0a5ad52d92bf4eb104a5e380e
doriansmiley <doriansmiley@siliconpublishing.com>
2022-10-01 09:04:18 -0700

    migrate to latest NX

--- apps/native-es-modules/jest.config.ts
+++ apps/native-es-modules/jest.config.ts
@@ -1,9 +1,10 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'native-es-modules',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
-    '^.+\\.[tj]s$': 'babel-jest',
+    '^.+\\.[tj]s$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/native-es-modules',
